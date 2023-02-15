9b755776d374fcfbbdf10789da4c9cd24d4095df
Luan Santos <git@luan.sh>
2023-02-14 21:12:21 -0800

    authorize users

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,19 +1,37 @@
 import { getUserFromToken } from '$lib/server/auth';
+import { prisma } from '$lib/server/prisma';
 import { redirect, type Handle } from '@sveltejs/kit';
 
 export const handle = (async ({ event, resolve }) => {
-	if (event.url.pathname.startsWith('/login') || event.url.pathname.startsWith('/callback/google')) {
-		const response = await resolve(event);
-		return response;
-	}
+  if (
+    event.url.pathname.startsWith('/login') ||
+    event.url.pathname.startsWith('/callback/google')
+  ) {
+    const response = await resolve(event);
+    return response;
+  }
 
-	const token = event.cookies.get('auth');
-	if (!token) {
-		throw redirect(302, '/login');
-	}
+  const token = event.cookies.get('auth');
+  if (!token) {
+    throw redirect(302, '/login');
+  }
 
-	event.locals.user = getUserFromToken(token);
+  const user = getUserFromToken(token);
+  if (!user) {
+    throw redirect(302, '/login');
+  }
+  console.log(user);
 
-	const response = await resolve(event);
-	return response;
+  event.locals.user = user;
+  const dbUser = await prisma.user.findUnique({
+    where: {
+      id: user.userId
+    }
+  });
+  if (!dbUser) {
+    throw redirect(302, '/login');
+  }
+
+  const response = await resolve(event);
+  return response;
 }) satisfies Handle;
