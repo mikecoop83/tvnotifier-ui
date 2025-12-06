3140210f17315136fd1d383ceeaa89c1257d1e39
Mike Coopersmith <coop@dropbox.com>
2025-12-06 12:31:29 -0500

    skeleton

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+    './node_modules/@skeletonlabs/skeleton/**/*.svelte'
+  ],
   theme: {
     extend: {}
   },
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
-    require('@tailwindcss/line-clamp')
-    // Skeleton plugins removed for now to avoid build issues
-    // ...require('@skeletonlabs/skeleton/tailwind/skeleton.cjs')()
+    require('@tailwindcss/line-clamp'),
+    require('@skeletonlabs/tw-plugin')
   ]
 };
